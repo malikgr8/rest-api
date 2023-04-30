0e124130d927923f3acf5c2a487bc8d173b70aa3
Malik <ahmad.cuteboy98@gmail.com>
2023-04-30 16:56:05 +0500

    Added profiles viewset with filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import serializers
 from rest_framework import viewsets
 from profiles_api import models
@@ -113,4 +114,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,) 
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',) 
